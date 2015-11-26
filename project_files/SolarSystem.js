381708ee7e0369d0346e53ed2b79d99577a01015
Broderick Beaman <broderick.beaman@unb.ca>
2015-11-26 18:05:17 -0400

    Drawing a sphere without a lighting model is complete (looks like circles).

--- project_files/SolarSystem.js
+++ project_files/SolarSystem.js
@@ -4,11 +4,13 @@ var VSHADER_SOURCE =
 	'attribute vec4 a_Position;\n' +
 	'uniform mat4 u_MvpMatrix;\n' +
 	'uniform mat4 u_ModelMatrix;\n' +    // Model matrix
+	'uniform vec3 u_LightPosition;\n' +  // Position of the light source
 	'varying vec4 v_Color;\n' +
 	'void main() {\n' +
 	'  vec4 color = vec4(1.0, 1.0, 1.0, 1.0);\n' + // Sphere Color
 	'  gl_Position = u_MvpMatrix * a_Position;\n' +
 	'  vec4 vertexPosition = u_ModelMatrix * a_Position;\n' +
+	'  vec3 lightDirection = normalize(u_LightPosition - vec3(vertexPosition));\n' +
 	'  v_Color = vec4(1.0, 1.0, 1.0, color.a);\n' +
 	'}\n';
 
@@ -40,8 +42,9 @@ function main() {
   
 	// Set the vertex coordinates and the color.
 	var n = initVertexBuffers(gl);
-	if (n < o) {
+	if (n < 0) {
 		console.log('Failed to set the vertex information');
+		return;
 	}
 	
 	// Set clear color and enable the depth test
@@ -50,11 +53,13 @@ function main() {
 	
 	// Get the storage locations of uniform variables
 	var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-	var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-	if (!u_ModelMatrix || !u_MvpMatrix) {
-		console.log('Failed to get the storage location of one or more uniform variables');
-		return;
-	}
+  	var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  	
+  	// UNCOMMENT THIS BLOCK WHEN IMPLEMENTING LIGHTING
+	//if (!u_ModelMatrix || !u_MvpMatrix) {
+	//	console.log('Failed to get the storage location of one or more uniform variables');
+	//	return;
+	//}
 	
 	// Various matrices
 	var modelMatrix = new Matrix4(); // Model Matrix
@@ -77,3 +82,97 @@ function main() {
 	// Draw the sphere(Note that the 3rd argument is the gl.UNSIGNED_SHORT)
 	gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_SHORT, 0);
 }
+
+// Code to create a sphere
+function initVertexBuffers(gl) {
+	// Increase to make spheres more smooth
+	var SPHERE_DIV = 13;
+	
+	var i, ai, si, ci;
+	var j, aj, sj, cj;
+	var p1, p2;
+	
+	var positions = [];
+	var indices = [];
+	
+	// Generate coordinates
+	for (j = 0; j <= SPHERE_DIV; j++) {
+		aj = j * Math.PI / SPHERE_DIV;
+		sj = Math.sin(aj);
+		cj = Math.cos(aj);
+		for (i = 0; i <= SPHERE_DIV; i++) {
+			ai = i * 2 * Math.PI / SPHERE_DIV;
+			si = Math.sin(ai);
+			ci = Math.cos(ai);
+			
+			positions.push(si * sj); // X
+			positions.push(cj); // Y
+			positions.push(ci * sj); // Z
+		}
+	}
+	
+	// Generate indices
+	for (j = 0; j < SPHERE_DIV; j++) {
+		for (i = 0; i < SPHERE_DIV; i++) {
+			p1 = j * (SPHERE_DIV + 1) + i;
+			p2 = p1 + (SPHERE_DIV + 1);
+			
+			indices.push(p1);
+			indices.push(p2);
+			indices.push(p1 + 1);
+			
+			indices.push(p1 + 1);
+			indices.push(p2);
+			indices.push(p2 + 1);
+		}
+	}
+	
+	// Write the vertex property to buffers
+	if (!initArrayBuffer(gl, 'a_Position', new Float32Array(positions), gl.FLOAT, 3)) return -1;
+	
+	// Unbind the buffer object
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+	
+	// Write the indices to the buffer object
+	var indexBuffer = gl.createBuffer();
+	
+	if (!indexBuffer) {
+		console.log('Failed to create the buffer object');
+		return -1;
+	}
+	
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+	
+	return indices.length;
+}
+
+function initArrayBuffer(gl, attribute, data, type, num) {
+	// Create a buffer object
+	var buffer = gl.createBuffer();
+	if (!buffer) {
+		console.log('Failed to create the buffer object');
+		return false;
+	}
+	
+	// Write data into the buffer object
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+	
+	// Assign the buffer object to the attribute variable
+	var a_attribute = gl.getAttribLocation(gl.program, attribute);
+	if (a_attribute < 0) {
+		console.log('Failed to get the storage location of ' + attribute);
+		return false;
+	}
+	
+	gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+	
+	// Enable the assignment of the buffer object to the attribute variable
+	gl.enableVertexAttribArray(a_attribute);
+	
+	gl. bindBuffer(gl.ARRAY_BUFFER, null);
+	
+	return true;
+}
+
