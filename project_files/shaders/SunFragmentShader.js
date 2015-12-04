4d3c8425e9fe8a1401e2072617c21c18ca5c26b5
supra <c9e51@unb.ca>
2015-12-03 23:44:27 -0400

    Temp changes for multiple light sources

--- project_files/shaders/SunFragmentShader.js
+++ project_files/shaders/SunFragmentShader.js
@@ -74,8 +74,8 @@ FSHADER_SOURCE[3] =
 	'#ifdef GL_ES\n' +
 	'precision mediump float;\n' +
 	'#endif\n' +
-	'uniform vec3 u_LightColor;\n' +     // Light color
-	'uniform vec3 u_LightPosition;\n' +  // Position of the light source
+	'uniform vec3 u_LightColor[];\n' +     // Light color
+	'uniform vec3 u_LightPosition[];\n' +  // Position of the light source
 	'uniform vec3 u_AmbientLight;\n' +   // Ambient light color
 	'varying vec3 v_Normal;\n' +
 	'varying vec3 v_Position;\n' +
@@ -83,12 +83,13 @@ FSHADER_SOURCE[3] =
 	'void main() {\n' +
 	   // Normalize the normal because it is interpolated and not 1.0 in length any more
 	'  vec3 normal = normalize(v_Normal);\n' +
-    // Calculate the light direction and make it 1.0 in length
-	'  vec3 lightDirection = normalize(u_LightPosition - v_Position);\n' +
+	'  for(int i = 0; i < u_LightPosition.length; i++){' +
+	'  	vec3 lightDirection = normalize(u_LightPosition[i] - v_Position);\n' +
     // The dot product of the light direction and the normal
-	'  float nDotL = max(dot(lightDirection, normal), 0.0);\n' +
+	'  	float nDotL += max(dot(lightDirection, normal), 0.0);\n' +
 	// Calculate the final color from diffuse reflection and ambient reflection
-	'  vec3 diffuse = u_LightColor * v_Color.rgb * nDotL;\n' +
+	'  	vec3 diffuse = u_LightColor[i] * v_Color.rgb * nDotL;\n' +
+	'  }' +
 	'  vec3 ambient = u_AmbientLight * v_Color.rgb;\n' +
 	'  gl_FragColor = vec4(diffuse + ambient, v_Color.a);\n' +
 	'}\n';
